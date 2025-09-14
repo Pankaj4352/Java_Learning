9991beccd81b64ff22a998f4fb0df1dfe7a32a2a
Pankaj Gupta <Guptapankaj4352@gmail.com>
2025-09-14 16:26:44 +0530

    How to user comment

---
+++ src/Java_Basic/E002_comment.java
@@ -0,0 +1,17 @@
+package Java_Basic;
+
+public class E002_comment {
+    public static void main(String[] args) {
+        // for single line comment
+        /*int a=1;
+        char c= 'p';
+        boolean b= true;
+        float i =124.54f;
+        long l= 7526428151L;
+        double j =645623.543d;
+        String s= "Rahul";
+        Multiple line command
+        */
+
+    }
+}
