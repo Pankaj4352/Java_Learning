0d070882a8c1b9a65a70437215edf22b79222b62
Pankaj Gupta <Guptapankaj4352@gmail.com>
2025-09-14 15:47:20 +0530

    Print 9 table using printf command.

--- src/Exercise/E01_Print_Number_using_printf.java
+++ src/Exercise/E01_Print_Number_using_printf.java
@@ -4,15 +4,15 @@ public class E01_Print_Number_using_printf {
     public static void main(String[] args) {
         int num=9;
         System.out.printf("%d x 1 = %d \n",num, num);
-        System.out.printf("%d x 1 = %d \n",num, num * 2);
-        System.out.printf("%d x 1 = %d \n",num, num* 3);
-        System.out.printf("%d x 1 = %d \n",num, num * 4);
-        System.out.printf("%d x 1 = %d \n",num, num * 5);
-        System.out.printf("%d x 1 = %d \n",num, num * 6);
-        System.out.printf("%d x 1 = %d \n",num, num* 7);
-        System.out.printf("%d x 1 = %d \n",num, num * 8);
-        System.out.printf("%d x 1 = %d \n",num, num * 9);
-        System.out.printf("%d x 1 = %d \n",num, num * 10);
+        System.out.printf("%d x 1 = %d \n",num, num*2);
+        System.out.printf("%d x 1 = %d \n",num, num*3);
+        System.out.printf("%d x 1 = %d \n",num, num*4);
+        System.out.printf("%d x 1 = %d \n",num, num*5);
+        System.out.printf("%d x 1 = %d \n",num, num*6);
+        System.out.printf("%d x 1 = %d \n",num, num*7);
+        System.out.printf("%d x 1 = %d \n",num, num*8);
+        System.out.printf("%d x 1 = %d \n",num, num*9);
+        System.out.printf("%d x 1 = %d \n",num, num*10);
 
 
 
