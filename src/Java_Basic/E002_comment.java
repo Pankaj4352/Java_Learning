b27856e7525ab0b385ec0fbe4f3f5ff28ce5eb51
Pankaj Gupta <Guptapankaj4352@gmail.com>
2025-09-14 16:29:46 +0530

    How to user comment command

--- src/Java_Basic/E002_comment.java
+++ src/Java_Basic/E002_comment.java
@@ -3,7 +3,8 @@ package Java_Basic;
 public class E002_comment {
     public static void main(String[] args) {
         // for single line comment
-        /*int a=1;
+        /*
+        int a=1;
         char c= 'p';
         boolean b= true;
         float i =124.54f;
