3e18ce1a27d98827261cb26d03bc349c02570b24
Pankaj Gupta <Guptapankaj4352@gmail.com>
2025-09-14 15:41:39 +0530

    Print 9 table using printf command.

---
+++ src/Exercise/E01_Print_Number_using_print.java
@@ -0,0 +1,20 @@
+package Exercise;
+
+public class E01_Print_Number_using_print {
+    public static void main(String[] args) {
+        int num=9,num1=9;
+        System.out.printf("%d x 1 = %d \n",num, num1);
+        System.out.printf("%d x 1 = %d \n",num, num1 * 2);
+        System.out.printf("%d x 1 = %d \n",num, num1* 3);
+        System.out.printf("%d x 1 = %d \n",num, num1 * 4);
+        System.out.printf("%d x 1 = %d \n",num, num1 * 5);
+        System.out.printf("%d x 1 = %d \n",num, num1 * 6);
+        System.out.printf("%d x 1 = %d \n",num, num1* 7);
+        System.out.printf("%d x 1 = %d \n",num, num1 * 8);
+        System.out.printf("%d x 1 = %d \n",num, num1 * 9);
+        System.out.printf("%d x 1 = %d \n",num, num1 * 10);
+
+
+
+    }
+}
